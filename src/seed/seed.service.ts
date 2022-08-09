99a472c60ed678f115a87557c4826797c8e04340
Alejo Boga <alejoboga20@gmail.com>
2022-08-09 14:41:42 -0300

    Seed DB

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
+import { ProductsService } from '../products/products.service';
+import { initialData } from './data/seed-data';
+
 @Injectable()
 export class SeedService {
+  constructor(private readonly productsService: ProductsService) {}
+
   async runSeed() {
+    this.insertNewProducts();
     return 'SEED EXECUTED';
+  }
+
+  private async insertNewProducts() {
+    const insertPromises = [];
+
+    await this.productsService.deleteAllProducts();
+
+    const seedProducts = initialData.products;
+    seedProducts.forEach((product) => {
+      insertPromises.push(this.productsService.create(product));
+    });
+
+    await Promise.all(insertPromises);
   }
 }
